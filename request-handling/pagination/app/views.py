c0cb47a62f2ae6601c2a33c83ebcbbc05c9dd781
jalaln06 <43106137+jalaln06@users.noreply.github.com>
2020-07-26 12:35:22 +0400

    Finished pagination

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,33 +1,32 @@
 from collections import defaultdict
 
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 import csv
-import os
+from django.conf import settings
+
+
 def index(request):
     return redirect(reverse(bus_stations))
 
 
 def bus_stations(request):
-    columns = defaultdict(list)  # each value in each column is appended to a list
-    with open( os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data-398-2018-08-30.csv'), newline='',encoding="cp1251") as csvfile:
+    with open('data-398-2018-08-30.csv', newline='', encoding="cp1251") as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:  # read a row as {column1: value1, column2: value2,...}
-            for (k, v) in row.items():  # go over each column name and value
-                columns[k].append(v)  # append the value into the appropriate list
-                # based on column name k
+        data = list(reader)
 
-    print(columns['District'][0])
-    print(columns['Name'][0])
-    print(columns['Street'][0])
-    current_page = 1
-    pages = request.GET['page']
-    next_page_url = 'write your url'
+    current_page = request.GET.get('page', 1)
+    # pages = request.GET['page']
+    paginator = Paginator(data, 20)
+    page_obj = paginator.get_page(current_page)
+    current_page=page_obj.number
+    next_page_url = f'?page={page_obj.next_page_number()}' if page_obj.has_next() else None
+    prev_page_url = f'?page={page_obj.previous_page_number()}' if page_obj.has_previous() else None
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': columns['Name'][0], 'Street': 'улица', 'District': 'район'}],
+        'bus_stations': page_obj.object_list,
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
-
